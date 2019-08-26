0eb22be2f9aa9f9139622fab6b252d43fb7c8f6e
Morgan Smith <morgansmith0219@gmail.com>
2019-08-26 21:27:43 +0000

    sections drag and drop

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -20,7 +20,7 @@ class Instructor::LessonsController < ApplicationController
   private
 
 
-   def require_authorized_for_current_lesson
+  def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
